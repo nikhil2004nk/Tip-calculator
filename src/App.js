ad1f4e1e8f655376d3a4aa66894b7b78e8f4c73d
nikhil2004nk <nikhil2004kushwaha@gmail.com>
2025-07-02 12:55:59 +0530

    Refactor App component to wrap headings in a div

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1>hell here!!</h1>;
+  return (
+    <div>
+      <h1>hell here!!</h1>;<h1>hell here!!</h1>;
+    </div>
+  );
 }
 
 export default App;
